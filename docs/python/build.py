0fae167588ea4bdf19ae629de2847148c0530152
Tom Nijboer <tom.nijboer@cimpress.com>
2026-01-28 19:57:50 +0100

    chore: update to latest dependencies

--- docs/python/build.py
+++ docs/python/build.py
@@ -1,10 +1,3 @@
-# /// script
-# requires-python = ">=3.12"
-# dependencies = [
-#     "clear-skies",
-#     "clear-skies-doc-builder",
-# ]
-# ///
 from __future__ import annotations
 
 import clearskies_doc_builder
